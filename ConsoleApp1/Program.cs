eb0555eac634b53a8dc2f79ac43208a472762950
Pratik Nadagouda <prnadago@microsoft.com>
2020-03-20 00:21:48 +0000

    new changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Microsoft People");
+            Console.WriteLine("Hello Microsoft People.  ");
         }
     }
 }
